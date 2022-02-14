58abeb145a70ecb722bc4c13bc68af859197e7cd
Austin Willoughby <arwigm@rit.edu>
2022-02-13 19:30:13 -0500

    Added intentionally broken code

---
+++ src/jsonResponses.js
@@ -0,0 +1,55 @@
+// Note this object is purely in memory
+// When node shuts down this will be cleared.
+const users = {};
+
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
+
+  respondJSON(request, response, 200, responseJSON);
+};
+
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
+
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 204;
+  if (!users[body.name]) {
+    responseCode = 201;
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    respondJSON(request, response, responseCode, responseJSON);
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+module.exports = {
+  getUsers,
+  addUser,
+};
---
+++ src/server.js
@@ -0,0 +1,64 @@
+/* Note: this repo is provided as starter code for an in-class debugging demo.
+   There is no "done" version with comments, since the debugging largely takes place
+   outside of the code. Refer to the "Debugging Node" document on MyCourses for tips
+   on how to debug this code. If you do wish to know more about this code, look at the
+   Body-Parse example in the class github organization.
+*/
+
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const parseBody = (request, response, handler) => {
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+    handler(request, response, bodyParams);
+  });
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    parseBody(request, response, jsonHandler.addUser);
+  }
+};
+
+const handleGet = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response);
+  } else {
+    htmlHandler.getIndex(request, response);
+  }
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
